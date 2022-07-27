9fbb379dfce2f58dc06e0ba02afb60e751e8489a
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 00:37:44 +0300

    data to pick from Api

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Movie = () => {
+const Movie = ({title, id, release_date, poster_path, overview, vote_average, vote_count}) => {
   return (
     <div>Movie</div>
   )
