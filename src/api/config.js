8f12d692f35c2fbd7b356b59eafb1c92eca6806f
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 00:51:50 +0300

    adding api

---
+++ src/api/config.js
@@ -0,0 +1,5 @@
+export const apiKey = "864abc344e6193436694c57936f427a3";
+export const MOVIES_API = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=`
+export const IMAGES_API = 'https://image.tmdb.org/t/p/w1280' 
+export const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${apiKey}&query=`
+export const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`
