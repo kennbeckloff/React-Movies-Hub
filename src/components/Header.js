7a7da45f05af7f18d67435ce5a8c67fc3b9efaad
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-27 16:54:35 +0300

    added Header

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+
+const Header = () => {
+  return (
+    <div  className="Navbar_field container">
+
+      <div  >
+          <Link to="/"  className="logo">
+            Movie-Hub
+          </Link>
+      </div>
+
+      <Form />
+
+      <ul>
+          <li >
+                 <Link to="/popular" className="popular">
+                    Popular
+                </Link>
+            </li>
+            <li >
+                <Link to="/myFavorites" className="myFavorites">
+                    My Favorites
+                </Link>
+            </li>
+            <li >
+                <Link to="/topSearches" className="topSearches">
+                    Top Searches
+                </Link>
+            </li>
+           
+        </ul>   
+
+    </div>
+  );
+};
+
+export default Header;
