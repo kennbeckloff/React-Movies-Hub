8fd3c2d5e4b71bcb86482b1cb3920a02777c27f4
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 22:19:59 +0300

    Added Ratings

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useState,useEffect } from "react";
+import {FaStar} from 'react-icons/fa'
+const StarRating = ({rate}) => {
+    const [rating , setRating ] = useState(0)
+    useEffect(() => {
+        setRating(rate)
+    }, [rate])
+        return (
+            <>
+            {
+                [...Array(5)].map((star,i) =>{
+                    const ratingVal = (i+1) *2 ;  
+                    return(
+                    <label   key={i}>
+                        <input type="radio" name="rating" value={ratingVal}  />
+                    <FaStar className="star" size={40} color={ratingVal <= rating ? '#A435F0' : 'black'}/>
+                    </label>
+                    )
+                }) 
+            }
+            </>
+        )
 
-const StarRating = () => {
-  return (
-    <div>StarRating</div>
-  )
 }
 
-export default StarRating
+
+export default StarRating ; 
