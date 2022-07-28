5028f9d92b884d2397688a6ebd43ab1114375bce
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 17:17:39 +0300

    Getting Popular movie

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -5,10 +5,10 @@ import { MoviesContext } from "../context/MoviesContext";
 import Loader from "./Loader";
 import Movie from "./Movie";
 
-const Popular = () => {
-  return (
-    <div>Popular</div>
-  )
-}
+const  Popular =() =>  {
+  const {movies,setMovies , loading , setLoading } = useContext(MoviesContext)  
+  useEffect(() => {      
+      getMovies(FEATURED_API)    
+  });
 
 export default Popular
