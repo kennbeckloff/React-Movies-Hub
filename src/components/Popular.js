234355b4696ec22d9f3a4f8a8ec876022245edd3
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 22:08:29 +0300

    Gettinf popular movie elements

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -6,9 +6,37 @@ import Loader from "./Loader";
 import Movie from "./Movie";
 
 const  Popular =() =>  {
-  const {movies,setMovies , loading , setLoading } = useContext(MoviesContext)  
-  useEffect(() => {      
-      getMovies(FEATURED_API)    
-  });
+    const {movies,setMovies , loading , setLoading } = useContext(MoviesContext)    
+    useEffect(() => {        
+        getMovies(FEATURED_API)      
+    });
 
-export default Popular
+    const getMovies = async (API) => {
+        const moviesJson = await axios(API);
+        setMovies(moviesJson.data.results)      
+        setTimeout(()=> {
+          setLoading(false);
+        },3000)
+    }
+    return (  
+
+        {  
+        <div className="movieList  container d-flex flex-wrap justify-content-center  mt-4" >  
+          {
+              loading ? <Loader/> : 
+           movies.map((movie) => {
+            return (
+              <Movie {...movie} key ={movie.id} />
+            )
+            }             
+            )          
+          }  
+        </div>        
+        }
+        
+            
+    )
+}
+
+
+export default Popular ; 
