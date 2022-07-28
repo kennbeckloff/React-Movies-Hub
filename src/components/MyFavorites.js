2ddad176acd3046f2f763c80c168d0b0ca8a6da5
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 21:59:04 +0300

    usefect

--- src/components/MyFavorites.js
+++ src/components/MyFavorites.js
@@ -7,6 +7,14 @@ import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios'
 
 const MyFavorites = () => {
+    const { myfavorites ,loading,setLoading} = useContext(MoviesContext)
+    const [favoriList, setFavoriList] = useState([])    
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000);
+        getMovie()
+    }, [setLoading])
   return (
     <div>MyFavorites</div>
   )
