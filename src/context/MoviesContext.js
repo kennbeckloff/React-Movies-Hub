473ad1bb586b81bb94b248f9647d394568b54d42
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 15:15:36 +0300

    degugging

--- src/context/MoviesContext.js
+++ src/context/MoviesContext.js
@@ -1,38 +1,59 @@
 import React, {  useState,useEffect } from "react";
 import axios from "axios";
 import { SEARCH_API } from "../api/config";
+
 export const   MoviesContext = React.createContext()  ;
 
 
 const MoviesContextProvider = props => {
+
   const [movies ,setMovies ] = useState([]) 
+
   const [myfavorites ,setMyfavorites] =useState([])
+
   const [loading, setLoading] = useState(true);
+
 
 
 
   useEffect(() => {
     const FavoriList = getMoviesFromStorage() ; 
+
     setMyfavorites(FavoriList)
+
   }, [])
 
 
 
 
-  const searchMovies = async (searchTerm) => {        
+  const searchMovies = async (searchTerm) => {
+
+        
     const searchRes =  await axios(SEARCH_API + searchTerm)
+
     setMovies(searchRes.data.results)
+
     setLoading(false);
 
   }
 
+
+
+
+
+
 const getMoviesFromStorage = () => {
+
+   
      if ( localStorage.getItem("myFavoriList") ) {
-        let FavoriList = JSON.parse(localStorage.getItem("myFavoriList"))        
+
+        let FavoriList = JSON.parse(localStorage.getItem("myFavoriList"))
+        
         return FavoriList
 
      }
      else {
+
         let FavoriList = []
         return FavoriList
       
