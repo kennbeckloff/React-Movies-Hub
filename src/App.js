40dea0d214418d6d02f59a06e3ee710ebf6dfa30
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 12:51:37 +0300

    Debugging APP.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
-import {  Route, Switch , BrowserRouter as Router } from "react-router-dom";
-import Header from "./components/Header";
-import MovieList from "./components/MovieList";
-import NotFound from "./components/NotFound";
+// Module not found: Error: Can't resolve 'react-router-dom' in '/home/pmm/Development/code/phase-2/movies-hub/src'
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 import Popular from "./components/Popular";
 import MyFavorites from "./components/MyFavorites";
+import MovieList from "./components/MovieList";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
 
-import Home from "./components/Home";
+
+// import {  Route, Switch , BrowserRouter as Router } from "react-router-dom";
+const Route = Requirements.Route;
+const Switch = Requirements.Switch;
+
+
 
 const  App =()  => {
   const handleSubmit = (e, history, searchInput) => {
