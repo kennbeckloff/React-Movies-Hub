1fee2a863ac668ebd8ee48971cd3a61c50049540
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 12:03:01 +0300

    Added Navbar and search css. Implemented get movie in home component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,52 @@ import React from "react";
 import {  Route, Switch , BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import MovieList from "./components/MovieList";
+import NotFound from "./components/NotFound";
 import Popular from "./components/Popular";
 import MyFavorites from "./components/MyFavorites";
 
 import Home from "./components/Home";
 
-function App() {
+const  App =()  => {
+  const handleSubmit = (e, history, searchInput) => {
+    e.preventDefault();
+    e.currentTarget.reset();
+    let url = `/search/${searchInput}`;
+    history.push(url);
+  };
+
   return (
-    <div className="App">
-     
-    </div>
+      <Router >
+        <div className="App">
+          <Route
+            render={props => (
+              <Header
+                handleSubmit={handleSubmit}
+                history={props.history}
+              />
+            )}
+          />
+          
+          <Switch>
+          < Route path="/" exact component={Home} />
+          < Route path="/popular" exact component={Popular} />         
+          < Route path="/myFavorites" exact component={MyFavorites} />
+           
+            <Route
+              path="/search/:searchInput"
+              render={props => (               
+                <MovieList searchTerm={props.match.params.searchInput} />
+              )}
+            />
+            <Route exact={true}  component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
 export default App;
+
+
+
+
