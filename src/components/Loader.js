356a677d44bff6aeae46bb5c472150d5960db8ab
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 15:54:05 +0300

    change css classes

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Loader = () => {
   return (
-    <div className="load">
-        <div className="loader"></div>
+     <div className="mask">
+         <div className="loader"></div>
     </div>
-  )
+
+  );
 }
 
-export default Loader
+export default Loader;
