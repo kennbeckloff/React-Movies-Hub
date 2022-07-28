8a133b392183aeb517ea5d5c27b1dc5756c565bf
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 21:57:04 +0300

    favourite imports

--- src/components/MyFavorites.js
+++ src/components/MyFavorites.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import { useContext, useState, useEffect } from "react";
+import { MoviesContext } from "../context/MoviesContext";
+import Loader from "./Loader";
+import Movie from "./Movie";
+import { apiKey } from "../api/config";
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios'
 
 const MyFavorites = () => {
   return (
