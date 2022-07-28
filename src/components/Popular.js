04f8e3a92fded8a56476518cc183194ada0af9e6
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-28 17:16:47 +0300

    Added popular imports

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import { FEATURED_API } from "../api/config";
+import React, {  useContext ,useEffect} from "react";
+import axios from "axios";
+import { MoviesContext } from "../context/MoviesContext";
+import Loader from "./Loader";
+import Movie from "./Movie";
 
 const Popular = () => {
   return (
