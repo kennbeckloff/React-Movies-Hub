623e442c2cf869511abb924c8f375ead363fcd4a
kennbeckloff <kennbeckloff688@gmail.com>
2022-07-29 13:51:46 +0300

    Debug Popular

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -6,33 +6,50 @@ import Loader from "./Loader";
 import Movie from "./Movie";
 
 const  Popular =() =>  {
-    const {movies,setMovies , loading , setLoading } = useContext(MoviesContext)    
-    useEffect(() => {        
-        getMovies(FEATURED_API)      
+
+    const {movies,setMovies , loading , setLoading } = useContext(MoviesContext)
+    
+    useEffect(() => {
+        
+        getMovies(FEATURED_API)
+
+      
     });
 
     const getMovies = async (API) => {
+
         const moviesJson = await axios(API);
-        setMovies(moviesJson.data.results)      
+
+        setMovies(moviesJson.data.results)
+      
         setTimeout(()=> {
+
           setLoading(false);
+
         },3000)
     }
-    return (  
-      <>
 
-        {  
-        <div className="movieList  container d-flex flex-wrap justify-content-center  mt-4" >  
+    return (
+        <>
+
+        {
+  
+        <div className="movieList  container d-flex flex-wrap justify-content-center  mt-4" >
+  
           {
               loading ? <Loader/> : 
            movies.map((movie) => {
             return (
               <Movie {...movie} key ={movie.id} />
             )
-            }             
-            )          
-          }  
-        </div>        
+            } 
+            
+            )
+          
+          }
+  
+        </div>
+        
         }
         </>
             
